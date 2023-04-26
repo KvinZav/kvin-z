027f6c4678d5bb8e13411e109b4082009ea25555
KevinZav <kevin.zavala@umvel.com>
2023-04-26 18:57:41 -0500

    drop files

--- projects/kvin-z/src/lib/kvin-z.module.ts
+++ projects/kvin-z/src/lib/kvin-z.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { KvinZComponent } from './kvin-z.component';
-import { MusicComponent } from './components/music/music.component';
 
 
 
 @NgModule({
   declarations: [
     KvinZComponent,
-    MusicComponent
   ],
   imports: [
   ],
